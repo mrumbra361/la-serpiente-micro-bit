ae5756c7821dbb544fecb2d30da6d3ca5f980eaf
mrumbra361 <122780690+mrumbra361@users.noreply.github.com>
2023-02-06 10:25:21 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
+})
+let lista = [game.createSprite(0, 2), game.createSprite(1, 2), game.createSprite(2, 2)]
+basic.forever(function () {
+    for (let index = 0; index <= 2; index++) {
+    	
+    }
 })
